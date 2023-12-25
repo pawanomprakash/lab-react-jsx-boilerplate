96483cd661729382ed9c1c633042e68de06eb672
pawanomprakash <141708273+pawanomprakash@users.noreply.github.com>
2023-12-25 23:44:13 +0530

    updated

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,10 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(() => {
   return {
     build: {
-      outDir: 'build',
+      outDir: 'dist',
     },
     plugins: [react()],
     base:'/lab-react-jsx-boilerplate/'
+
   };
 });
